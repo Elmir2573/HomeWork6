48f038b136cffcaf8e9b5d86f94c7ff34c04d0f5
zzr-i@mail.ru <zzr-i@mail.ru>
2024-12-06 22:25:42 +0300

    HW6 на проверку 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задание 1");
+        System.out.println("Задание #1");
         for (int i = 1; i <= 10; i++)
             System.out.println(i);
         System.out.println("Задание 2");
