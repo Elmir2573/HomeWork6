8f2ae7cf965972fcb8312a2d7f46b87ff300c772
zzr-i@mail.ru <zzr-i@mail.ru>
2024-12-06 22:20:05 +0300

    HW6 на проверку 2

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,24 @@ public class Main {
         System.out.println("Задание 1");
         for (int i = 1; i <= 10; i++)
             System.out.println(i);
-
         System.out.println("Задание 2");
         for (int i = 10; i >= 1; i = i - 1)
             System.out.println(i);
-
         System.out.println("Задание 3");
         for (int i = 0; i <= 17; i = i + 2)
             System.out.println(i);
-
         System.out.println("Задание 4");
         for (int i = 10; i >= -10; i = i - 1)
             System.out.println(i);
-
         System.out.println("Задание 5");
         for (int i = 1904; i <= 2096; i = i + 4)
             System.out.println(+i + " Год является високосным");
-
         System.out.println("Задание 6");
         for (int i = 7; i <= 98; i = i + 7)
             System.out.println(i);
-
         System.out.println("Задание 7");
         for (int i = 1; i <= 512; i = i * 2)
             System.out.println(i);
-
         System.out.println("Задание 8");
         int salary = 29000;
         int total = 0;
@@ -37,7 +30,6 @@ public class Main {
             total = total + salary;
             System.out.println("Месяц " + (i++) + ", сумма накоплений равна " + total + " рублей");
         }
-
         System.out.println("Задание 9");
         int salary2 = 29000;
         int total2 = 0;
